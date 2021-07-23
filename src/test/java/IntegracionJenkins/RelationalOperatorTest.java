03a8a7f8585349cba76ce79677cc6f514235ac0a
Jonathan Santana <jonfesan@gmail.com>
2021-07-23 15:32:39 -0500

    Corrigiendo y agregando Test

--- src/test/java/IntegracionJenkins/RelationalOperatorTest.java
+++ src/test/java/IntegracionJenkins/RelationalOperatorTest.java
@@ -19,14 +19,26 @@ public class RelationalOperatorTest {
 	}
 
 	@Test
+	public void isGreaterTest3() {
+		RelationalOperator tester = new RelationalOperator();
+		assertFalse(tester.isGreater(3, 3));
+	}
+
+	@Test
 	public void isLessTest1() {
 		RelationalOperator tester = new RelationalOperator();
-		assertTrue(tester.isLess(4, 4));
+		assertFalse(tester.isLess(4, 4));
 	}
 	
 	@Test
 	public void isLessTest2() {
 		RelationalOperator tester = new RelationalOperator();
 		assertFalse(tester.isLess(5, 1));
+	}
+
+	@Test
+	public void isLessTest3() {
+		RelationalOperator tester = new RelationalOperator();
+		assertTrue(tester.isLess(2, 6));
 	}
 }
